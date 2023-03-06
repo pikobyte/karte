9be634c08bdeeb7e7476a631dc52650eb290fe98
pikobyte <anthony.mercer@hotmail.co.uk>
2023-03-06 14:58:49 +0000

    More reformatting, use C23 attributes

--- include/core/resourcer.h
+++ include/core/resourcer.h
@@ -28,23 +28,23 @@
  * The resourcer stores vectors of different resources such as textures. Other
  * types of resource can be added if required.
  */
-typedef struct Resourcer_s
+typedef struct [[nodiscard]]
 {
-    Hashmap *textures; /**< Textures used by the program. */
+    Hashmap* textures; /**< Textures used by the program. */
 } Resourcer;
 
 /**
  * \brief Allocates memory for the resourcer.
  * \returns A pointer to a resourcer object.
  */
-Resourcer *ResourcerCreate(void);
+[[nodiscard]] Resourcer* ResourcerCreate(void);
 
 /**
  * \brief Frees the resourcer memory.
  * \param [in, out] res The resourcer to be freed.
  * \returns Void.
  */
-void ResourcerFree(Resourcer *res);
+void ResourcerFree(Resourcer* res);
 
 /**
  * \brief Loads a texture into memory.
@@ -54,14 +54,15 @@ void ResourcerFree(Resourcer *res);
  * \param [in] key An associated key for later look-up.
  * \returns Void.
  */
-void ResourcerLoadTexture(Resourcer *res, const Window *wind, const char *path,
-                          const char *key);
+void ResourcerLoadTexture(Resourcer* res, const Window* wind, const char* path,
+                          const char* key);
 
 /**
  * \brief Retrieves a texture from the resourcer.
  * \param [in] res The resourcer to retrieve the texture from.
  * \param [in] key The name of the texture to retrieve.
  */
-Texture *ResourcerGetTexture(const Resourcer *res, const char *key);
+[[nodiscard]] Texture* ResourcerGetTexture(const Resourcer* res,
+                                           const char* key);
 
 #endif
